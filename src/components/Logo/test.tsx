465830402432ffe3a2cf2a091d45d4c3fad0215d
isasakis <isabellasakis@gmail.com>
2023-12-20 12:20:16 -0300

    Create tests about prop size of logo

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -17,4 +17,18 @@ describe('<Logo />', () => {
       color: '#030517'
     })
   })
+
+  it('should render a big logo', () => {
+    renderWithTheme(<Logo size="large" />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      width: '20rem'
+    })
+  })
+
+  it('should render a normal logo', () => {
+    renderWithTheme(<Logo size="normal" />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      width: '11rem'
+    })
+  })
 })
